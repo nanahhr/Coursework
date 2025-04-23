39f07d1302da7f0a34310c0ca8bb9d5f5c8b9da1
nastasia <tv42.grigorenko.anastasia@gmail.com>
2025-04-23 23:29:55 +0300

    Update grid.cpp

--- grid.cpp
+++ grid.cpp
@@ -1,48 +1,115 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+#include "grid.h"
 
-struct Hint {
-    int row, col;
-    int count;
-    char direction;
-	Hint(int r, int c, int cn, char dir) : row(r), col(c), count(cn), direction(dir) {}
-};
-enum class CellState{EMPTY, FILLED, HINT};
+//Конструктор з параметрами для підказок.
+Hint::Hint(int r, int c, int cn, char dir) : 
+	row(r), col(c), count(cn), direction(dir) {}
 
-class Grid{
-	vector<CellState> cells;
-	vector<Hint> hints;
-	int rows, cols;
-	bool is_solved;
-public:
-	Grid() : rows(0), cols(0), is_solved(false) {}
-	Grid(int r, int c) 
-    : cells(r, vector<CellState>(c, CellState::EMPTY)),
-      rows(r), cols(c), is_solved(false) {}
-	Grid(const Grid& other)
-    : cells(other.cells),
-      hints(other.hints),
-      rows(other.rows),
-      cols(other.cols),
-      is_solved(other.is_solved) {}
-	  
-	 
-	int get_rows(){return rows;}
-	int get_cols(){return cols;}
-	CellState get_cell(int row, int col){return cells[row][col]}
-	vector<Hint>& get_hints(){return hints;}
-	bool get_is_solved(){return is_solved;}
+//Контруктор без параметрів для сітки.
+Grid::Grid() : rows(0), cols(0), is_solved(false) {}
+
+//Конструктор з параметрами(рядки і стовпці).
+Grid::Grid(int r, int c) 
+	: rows(r),
+	cols(c),
+	is_solved(false),
+	cells(r, vector<CellState>(c, CellState::EMPTY)) {}
+	   
+//Повертає кількість рядків сітки.  
+int Grid::get_rows(){return rows;}
 	
+//Повертає кількість стовпців сітки.
+int Grid::get_cols(){return cols;}
 	
-	void set_cell(int row, int col, CellState state){cells[row][col] = state;}
-	void add_hint(Hint& hint){
+//Повертає клітинки.
+CellState Grid::get_cell(int row, int col){return cells[row][col]}
+	
+//Повертає вектор з підказками.
+vector<Hint>& Grid::get_hints(){return hints;}
+	
+//Повертає стан вирішення сітки.
+bool Grid::get_is_solved(){return is_solved;}
+	
+//Встановлює значення клітинки сітки.	
+void Grid::set_cell(int row, int col, CellState state){cells[row][col] = state;}
+	
+//Додає підказку для сітки.
+void Grid::add_hint(Hint& hint){
+	hints.push_back(hint);
+	cells[hint.row][hint.col] = CellState::HINT;
+}
+//Встановлює стан вирішення сітки.
+void Grid::set_is_solved(bool solved){is_solved = solved;}
+	
+//Перевіряє, чи координати клітинки є в межах сітки.
+void Grid::is_cell_valid(int row, int col){
+	return row >= 0 && row < rows && col >= 0 && col < cols;
+}
+	
+//Функція для отримання данних з файлу.
+void Grid::load_from_file(string& file_name){
+	ifstream file(file_name);
+		
+	if(!file){
+		cerr << "Cannot open file.\n";
+		return 1;
+	}
+	file >> rows >> cols;
+	cells.resize(rows, vector<CellState>(cols, CellState::EMPTY));
+			
+	int hints_count;
+	file >> hints_count;
+			
+	for (int i = 0; i < hints_count; ++i) {
+		Hint hint;
+		char direction;
+		file >> hint.row >> hint.col >> hint.value >> direction;
+
+		if (hint.row < 0 || hint.row >= rows || 
+			hint.col < 0 || hint.col >= cols) {
+			cerr << "Hint is out of range.";
+		}
+
+		hint.direction = toupper(direction);
 		hints.push_back(hint);
 		cells[hint.row][hint.col] = CellState::HINT;
 	}
-	void set_is_solved(bool solved){is_solved = solved;}
+}
+
+//Функція для отримання данних з консолі.
+void Grid::input_from_console() {
+		
+    cout << "Enter grid size (rows columns): ";
+    while (!(cin >> rows >> cols) || rows < 1 || cols < 1) {
+        cerr << "Enter two positive numbers.\n";
+        cin.clear();
+        cin.ignore(1000, '\n');
+    }
+    cells.assign(rows, vector<CellState>(cols, CellState::EMPTY));
 	
-	void is_cell_valid(int row, int col){
-		return row >= 0 && row < rows && col >= 0 && col < cols;
-	}
-};	
+    cout << "Number of hints: ";
+    int n;
+    while (!(cin >> n) || n < 0) {
+        cerr << "Invalid number of hints. Enter a number >= 0\n";
+        cin.clear();
+        cin.ignore(1000, '\n');
+    }
+
+    for (int i = 0; i < n; ++i) {
+        Hint h;
+        char dir;
+        
+        cout << "Hint №" << i+1 << " (row column number direction): ";
+        while (!(cin >> h.row >> h.col >> h.value >> dir) || 
+               h.row < 0 || h.row >= rows || 
+               h.col < 0 || h.col >= cols ||
+               (dir = tolower(dir), !strchr("udlr", dir))) {
+            cerr << "Input error. Try again.\n";
+            cin.clear();
+            cin.ignore(1000, '\n');
+        }
+        hint.direction = direction;
+
+        hints.push_back(hint);
+        cells[hint.row][hint.col] = CellState::HINT;
+    }
+};
