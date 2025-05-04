a4441f3de5b329441207cfcd6b27741e605937b4
nastasia <tv42.grigorenko.anastasia@gmail.com>
2025-05-04 18:10:46 +0300

    Update grid.cpp

--- grid.cpp
+++ grid.cpp
@@ -1,8 +1,8 @@
 #include "grid.h"
 
 //Конструктор з параметрами для підказок.
-Hint::Hint(int r, int c, int cn, char dir) : 
-	row(r), col(c), count(cn), direction(dir) {}
+Hint::Hint(int r, int c, int num, char dir) : 
+	row(r), col(c), num(n), direction(dir) {}
 
 //Контруктор без параметрів для сітки.
 Grid::Grid() : rows(0), cols(0), is_solved(false) {}
@@ -73,15 +73,15 @@ void Grid::load_from_file(string& file_name){
 			
 	for (int i = 0; i < hints_count; ++i) {
 		Hint hint;
-		char direction;
-		file >> hint.row >> hint.col >> hint.value >> direction;
+		char dir;
+		file >> hint.row >> hint.col >> hint.num >> dir;
 
 		if (hint.row < 0 || hint.row >= rows || 
 			hint.col < 0 || hint.col >= cols) {
 			cerr << "Hint is out of range.";
 		}
 
-		hint.direction = toupper(direction);
+		hint.direction = toupper(dir);
 		hints.push_back(hint);
 		cells[hint.row][hint.col] = CellState::HINT;
 	}
@@ -111,7 +111,7 @@ void Grid::input_from_console() {
         char dir;
         
         cout << "Hint №" << i+1 << " (row column number direction): ";
-        while (!(cin >> h.row >> h.col >> h.value >> dir) || 
+        while (!(cin >> h.row >> h.col >> h.num >> dir) || 
                h.row < 0 || h.row >= rows || 
                h.col < 0 || h.col >= cols ||
                (dir = tolower(dir), !strchr("udlr", dir))) {
@@ -124,4 +124,45 @@ void Grid::input_from_console() {
         hints.push_back(hint);
         cells[hint.row][hint.col] = CellState::HINT;
     }
-};
+}
+
+//Функція для виведення сітки.
+void Grid::print_grid() {
+	cout << "      ";
+	for (int i = 0; i < col; ++i) cout << setw(2) <<  i << "  ";
+	cout << "\n     ";
+	for (int i = 0; i < col; ++i) cout << "----";
+	cout << endl;
+
+	for (int i = 0; i < row; i++) {
+		cout << setw(3) <<  i << " " << "| ";
+		for (int j = 0; j < col; j++) {
+			switch (cells[i][j]) {
+				case EMPTY: cout << " · "; break;
+				case FILLED: cout << " # "; break;
+				case HINT: {
+					for (const Hint& h : hints) {
+						if (h.r == i && h.c == j) {
+							cout << setw(2) << h.num;
+							switch (h.dir) {
+								case 'r': cout << "→"; break;
+								case 'l': cout << "←"; break;
+								case 'u': cout << "↑"; break;
+								case 'd': cout << "↓"; break;
+								default: cout << "?"; break;
+							}
+							break;
+						}
+					}
+					break;
+				}
+				case LINE: cout << " + "; break;
+			}
+			cout << " ";
+		}
+		cout << "|" << endl;
+	}
+	cout << "     ";
+	for (int i = 0; i < col; i++) cout << "----";
+	cout << endl;
+}
